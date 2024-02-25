7625e1db1132a8315dbea8b88bd4c3e3fcea1882
aliozsoy1 <aliozsoy237@gmail.com>
2024-02-26 01:05:08 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminDashboard from '/src/AdminDashboard.jsx';
-import Portfolio from '/src/Portfolio.jsx';
-import ProjectDetail from '/src/ProjectDetail.jsx';
+import AdminDashboard from './AdminDashboard.jsx';
+import Portfolio from './Portfolio.jsx';
+import ProjectDetail from './ProjectDetail.jsx';
 
 const App = () => {
   return (
